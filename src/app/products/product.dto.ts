5d5baace1288a334f807eb1df82829e5a322c8cf
Enrique1311 <ejspinelli131176@gmail.com>
2024-12-02 21:35:33 -0300

    Utility types (Partial and Required)

--- src/app/products/product.dto.ts
+++ src/app/products/product.dto.ts
@@ -1,6 +1,14 @@
 import { Product } from './product.model';
 
+// Omit: omite los campos que no necesite
 export interface CreateProductDto
   extends Omit<Product, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
   categoryId: string;
 }
+
+// Pick: elige los campos que yo quiera que se incluyan
+type example = Pick<Product, 'color' | 'description'>;
+
+// Partial: genera otro tipo (de Product) con los campos opcionales.
+// Como si los hubiésemos tipado con "?"
+export interface UpdateProductDto extends Partial<CreateProductDto> {}
