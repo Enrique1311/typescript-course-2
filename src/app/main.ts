f5fbfa37ee9c3623e58b6ccfbf97782ce12752df
Enrique1311 <ejspinelli131176@gmail.com>
2024-12-02 21:03:51 -0300

    Omit and Pick

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,25 +4,16 @@ import { addProduct, products } from './products/product.service';
 
 for (let index = 0; index < 50; index++) {
   addProduct({
-    id: faker.string.uuid(),
     image: faker.image.url(),
     description: faker.commerce.productDescription(),
     title: faker.commerce.product(),
     price: parseInt(faker.commerce.price()),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
     color: faker.color.human(),
     size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
     stock: faker.number.int({ min: 1, max: 100 }),
     isNew: faker.datatype.boolean(),
     tags: faker.helpers.arrayElements(['A', 'B', 'C', 'D']),
-
-    category: {
-      id: faker.string.uuid(),
-      name: faker.commerce.department(),
-      createdAt: faker.date.recent(),
-      updatedAt: faker.date.recent(),
-    },
+    categoryId: faker.string.uuid(),
   });
 }
 console.log(products);
